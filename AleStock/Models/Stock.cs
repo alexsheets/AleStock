8bfad4557f58d4b03048999e5fee9bd94d619d8e
Alex Sheets <asheet3@lsu.edu>
2024-11-07 14:50:51 -0600

    Add project files.

---
+++ AleStock/Models/Stock.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AleStock.Models
+{
+    public class Stock
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Ticker { get; set; }
+        public string Quarter { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ AleStock/Models/StockEconomicalInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AleStock.Models
+{
+    public class StockEconomicalInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        // relation back to stock table
+        public int StockId { get; set; }
+
+        // company balances
+        public int TotalAssets { get; set; }
+        public int TotalLiabilities { get; set; }
+        public int TotalEquity { get; set; }
+
+        // metrics
+        public int GrossProfitMargin { get; set; }
+        public int OperatingMargin { get; set; }
+        public int NetProfitMargin { get; set; }
+        public int ReturnOnEquity { get; set; }
+        public int ReturnOnAssets { get; set; }
+        public int ReturnOnInvested { get; set; }
+        public float LiquidityRatio { get; set; }
+        public float LiabilitiesToEquityRatio { get; set; }
+        public float DebtRatio { get; set; }
+        public float DividendPayoutRatio { get; set; }
+
+        // company cash flow
+        public int NetCashOperating { get; set; }
+        public int NetCashInvesting { get; set; }
+        public int NetCashFinancing { get; set; }
+        public int NetCashDelta { get; set; }
+    }
+}
