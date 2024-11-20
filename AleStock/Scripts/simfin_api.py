db6b2cf1890a1c2c00ab3e40645c31dc478e75b6
Alex Sheets <asheet3@lsu.edu>
2024-11-20 13:34:23 -0600

    changes to simfin API class

--- AleStock/Scripts/simfin_api.py
+++ AleStock/Scripts/simfin_api.py
@@ -1,5 +1,5 @@
 import json
-import urllib.request
+from urllib.request import Request, urlopen
 
 class SimFinAPI:
 
@@ -16,16 +16,19 @@ class SimFinAPI:
             'Authorization': self.key
         }
         # create param string to append to url for GET request
-        params = f'?ticker={ticker}&statements={statement_type}&year={year}&period={period}'
+        params = f'?ticker={ticker}&statements={statement_type}&fyear={year}&period={period}'
         # create link to backend with params appended
         req_link = 'https://backend.simfin.com/api/v3/companies/statements/compact/{}'.format(params)
-        # retrieve response
-        response = urllib.request.urlopen(req_link, headers=headers, params=params)  
+
+        req = Request(req_link)
+        req.add_header('Accept', 'application/json, text/plain, */*')
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36')
+        req.add_header('Authorization', self.key)
         
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise Exception(f'Request failed, status code: {response.status_code}')
+        content = urlopen(req)
+        data = json.load(content)
+        
+        return data
         
     # function for extracting the data of separate kinds of statements and returning the json
     def pull_data(self, data):
@@ -44,19 +47,17 @@ class SimFinAPI:
     # create function which will aggregate the data and build a summary ubiquitously 
     def build_json(self, data, columns):
         summary = {}
-        for column, keys in columns.items():
-            summary[column] = {}
+        for category, keys in columns.items():
+            summary[category] = {}
             for key in keys:
-                val = data.get(key)
-                if isinstance(val, (int, float)):
-                    value = f"{value:,}"
-                elif val is None:
-                    value = "N/A"
+                value = data.get(key)
+                if value is None:
+                    formatted_value = "N/A"
+                elif isinstance(value, (int, float)):
+                    formatted_value = f"{value:,}"
                 else:
-                    value = val
-                
-                summary[column][key] = value
-         
+                    formatted_value = value
+                summary[category][key] = formatted_value
         return summary
                     
     # function to call and retrieve results from all finance web pulls
