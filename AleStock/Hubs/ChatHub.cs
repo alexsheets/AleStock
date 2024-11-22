0e40d87482c858c4ac67ffb8c7c84dce539e68bd
Alex Sheets <asheet3@lsu.edu>
2024-11-22 10:02:39 -0600

    changes to chat component of application

--- AleStock/Hubs/ChatHub.cs
+++ AleStock/Hubs/ChatHub.cs
@@ -12,9 +12,38 @@ namespace AleStock.Hubs
             await Clients.All.SendAsync("ReceiveMsg", "admin", $"{conn.User} has joined! Please be kind to them.");
         }
 
+        [HubMethodName("SendMsg")]
         public async Task SendMsg(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMsg", user, message);
+        }
+
+        [HubMethodName("SendPrivateMsg")]
+        public async Task SendPrivateMsg(string user, string connId, string message)
+        {
+            await Clients.User(user).SendAsync("SendPrivateMsg", message);
+            // await Clients.Client(connId).SendAsync("SendPrivateMsg", user, connId, message);
+
+        }
+
+        [HubMethodName("SendMsgToGroup")]
+        public async Task SendMsgToGroup(string gName, string user, string message)
+        {
+            await Clients.Group(gName).SendAsync("SendMsg", $"{Context.ConnectionId}-{user}: {message}");
+        }
+
+        [HubMethodName("AddToGroup")]
+        public async Task AddToGroup(string gName, string user)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, gName);
+            await Clients.Group(gName).SendAsync("SendMsg", $"{user} : has joined {gName}!");
+        }
+
+        [HubMethodName("RemoveFromGroup")]
+        public async Task RemoveFromGroup(string groupName, string user)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("SendMsg", $"{user} has left the group {groupName}");
         }
 
     }
