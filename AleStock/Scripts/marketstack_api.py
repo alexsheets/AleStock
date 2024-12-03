5008061e9653ceb6cdc209bb35f642848948ea5c
Alex Sheets <asheet3@lsu.edu>
2024-12-03 11:08:21 -0600

    adding marketstack funcs

--- AleStock/Scripts/marketstack_api.py
+++ AleStock/Scripts/marketstack_api.py
@@ -9,4 +9,14 @@ class MarketStackAPI:
     def __init__(self, key):
         self.key = key
 
-    
+    def build_url_eod(self, ticker, date_start, date_end):
+        # create param string to append to url for GET request
+        params = f'?symbols={ticker}&date_from={date_start}&date_to={date_end}&sort=asc&access_key={self.key}'
+        # create link to backend with params appended
+        req_link = 'https://api.marketstack.com/v1/eod{}'.format(params)
+        
+    def build_url_intraday(self, ticker, date_start, date_end):
+        # create param string to append to url for GET request
+        params = f'?symbols={ticker}&date_from={date_start}&date_to={date_end}&sort=asc&access_key={self.key}'
+        # create link to backend with params appended
+        req_link = 'https://api.marketstack.com/v1/intraday{}'.format(params)
