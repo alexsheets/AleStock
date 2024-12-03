e23e0e0c1c5148611876001be8988dffd417f1da
Alex Sheets <asheet3@lsu.edu>
2024-12-03 13:21:39 -0600

    created functions to pull info from marketstack

--- AleStock/Scripts/marketstack_api.py
+++ AleStock/Scripts/marketstack_api.py
@@ -9,14 +9,41 @@ class MarketStackAPI:
     def __init__(self, key):
         self.key = key
 
-    def build_url_eod(self, ticker, date_start, date_end):
-        # create param string to append to url for GET request
-        params = f'?symbols={ticker}&date_from={date_start}&date_to={date_end}&sort=asc&access_key={self.key}'
-        # create link to backend with params appended
+    def get_json_eod(self, ticker, date_start, date_end):
+        params = f'?access_key={self.key}&symbols={ticker}&date_from={date_start}&date_to={date_end}&sort=asc'
         req_link = 'https://api.marketstack.com/v1/eod{}'.format(params)
         
-    def build_url_intraday(self, ticker, date_start, date_end):
-        # create param string to append to url for GET request
-        params = f'?symbols={ticker}&date_from={date_start}&date_to={date_end}&sort=asc&access_key={self.key}'
-        # create link to backend with params appended
+        req = Request(req_link)
+        req.add_header('Accept', 'application/json, text/plain, */*')
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36')
+        
+        content = urlopen(req)
+        data = json.load(content)
+        
+        return data
+        
+    def get_json_intraday(self, ticker, date_start, date_end):
+        params = f'?access_key={self.key}&symbols={ticker}&date_from={date_start}&date_to={date_end}&sort=asc'
         req_link = 'https://api.marketstack.com/v1/intraday{}'.format(params)
+        
+        req = Request(req_link)
+        req.add_header('Accept', 'application/json, text/plain, */*')
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36')
+        
+        content = urlopen(req)
+        data = json.load(content)
+        
+        return data
+    
+    def get_json_splits(self, ticker):
+        params = f'?access_key={self.key}&symbols={ticker}'
+        req_link = 'https://api.marketstack.com/v1/splits{}'.format(params)
+        
+        req = Request(req_link)
+        req.add_header('Accept', 'application/json, text/plain, */*')
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36')
+        
+        content = urlopen(req)
+        data = json.load(content)
+        
+        return data
