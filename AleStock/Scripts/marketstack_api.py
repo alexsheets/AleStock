2ec58f4876f520c0f591c5759f6081185cff1a0c
Alex Sheets <asheet3@lsu.edu>
2024-12-03 11:03:52 -0600

    add marketstack API file

---
+++ AleStock/Scripts/marketstack_api.py
@@ -0,0 +1,12 @@
+﻿import json
+from urllib.request import Request, urlopen
+from datetime import date as datemethod
+from datetime import timedelta
+
+class MarketStackAPI:
+    
+    # initialize marketstack class using api key
+    def __init__(self, key):
+        self.key = key
+
+    
