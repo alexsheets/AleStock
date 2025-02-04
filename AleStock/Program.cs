2aa05eeb8b66e7f7975767bfa2da88cc235a5f72
Alex Sheets <asheet3@lsu.edu>
2025-02-04 10:05:15 -0600

    added react packages and setup for dotnet project for future usage

--- AleStock/Program.cs
+++ AleStock/Program.cs
@@ -4,10 +4,22 @@ using Newtonsoft.Json.Serialization;
 using dotenv;
 using dotenv.net;
 
+// added for frontend
+using React.AspNet;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using FluentAssertions.Common;
+using Microsoft.AspNetCore.Mvc;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // add c# chat package
 builder.Services.AddSignalR();
+
+// add react/frontend services
+builder.Services.AddReact();
+builder.Services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName).AddV8();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddNewtonsoftJson(o => o.SerializerSettings.ContractResolver = new DefaultContractResolver());
@@ -15,7 +27,6 @@ builder.Services.AddControllersWithViews().AddNewtonsoftJson(o => o.SerializerSe
 builder.Services.AddRazorPages();
 builder.Services.AddKendo();
 builder.Services.AddControllers();
-// builder.Services.AddServerSideBlazor();
 builder.Services.AddMvc();
 builder.Services.AddSession(o => o.IdleTimeout = TimeSpan.FromMinutes(500));
 builder.Services.AddHttpContextAccessor();
@@ -26,18 +37,14 @@ DotEnv.Load();
 var url = Environment.GetEnvironmentVariable("SUPABASE_URL");
 var key = Environment.GetEnvironmentVariable("SUPABASE_KEY");
 
+// instantiate supabase client to access throughout project
 var options = new Supabase.SupabaseOptions
 {
     AutoConnectRealtime = true,
     AutoRefreshToken = true,
 };
-
-//builder.Services.AddSingleton(_ => new Supabase.Client(url, key, options));
-
-// how to access throughout project?
 var supabase = new Supabase.Client(url, key, options);
 await supabase.InitializeAsync();
-
 
 var app = builder.Build();
 
@@ -49,21 +56,28 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseSession();
-app.UseRouting();
-app.MapRazorPages();
-
-app.UseAuthorization();
-app.UseAuthentication();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 // setup for chat endpoint
 app.MapHub<ChatHub>("/Chat");
+
+// setup for adding react files
+app.UseReact(config =>
+{
+    // pulls from /scripts/ subdir in wwwroot
+    config.AddScript("~/scripts/Example.jsx");
+});
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+app.UseSession();
+app.UseRouting();
+app.MapRazorPages();
+
+app.UseAuthorization();
+app.UseAuthentication();
 
 
 app.Run();
---
+++ AleStock/wwwroot/scripts/Example.jsx
@@ -0,0 +1,9 @@
+﻿class Example extends React.Component {
+    render() {
+        return (<h1>ReactJS test</h1>);
+    }
+}
+ReactDOM.render(
+    <Example />,
+    document.getElementById('divMessage')
+); 
